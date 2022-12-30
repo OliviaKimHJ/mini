71eba667147d73c59bb4dfda7fc12e7fa164e13d
KHJ <wemuhyen@naver.com>
2022-12-30 18:13:02 +0900

    첫번째

---
+++ JS/index_script.js
@@ -0,0 +1,31 @@
+// 네비바
+const navToggle =document.querySelector('.navbar-toggler');
+const listGShow = document.querySelector('.list-group');
+
+// 커리셀 
+let no =1;
+let carousel = document.querySelector('.carousel');
+// 네비바 클릭 함수
+navToggle.addEventListener('click',function(){
+    listGShow.classList.toggle('show');
+    listGShow.style.display = ((document.querySelector('.list-group').style.display!='none') ? 'none' : 'block');
+});
+
+document.querySelector('.btn1').addEventListener('click', function(){
+    carousel.style.transform = 'translateX(0)';
+    document.querySelector('.btn1').style.background ='#263238';
+    document.querySelector('.btn2').style.background ='white';
+    document.querySelector('.btn3').style.background ='white';
+});
+document.querySelector('.btn2').addEventListener('click', function(){
+    carousel.style.transform = 'translateX(-100vw)';
+    document.querySelector('.btn2').style.background ='#263238';
+    document.querySelector('.btn1').style.background ='white';
+    document.querySelector('.btn3').style.background ='white';
+});
+document.querySelector('.btn3').addEventListener('click', function(){
+    carousel.style.transform = 'translateX(-200vw)';
+    document.querySelector('.btn3').style.background ='#263238';
+    document.querySelector('.btn1').style.background ='white';
+    document.querySelector('.btn2').style.background ='white';
+});
